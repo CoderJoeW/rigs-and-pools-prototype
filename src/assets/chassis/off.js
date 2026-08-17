4506b528700ca1afb5f3f6050eb265cc29d8f904
Joe <j.williamson@gambler-labs.com>
2026-08-17 17:34:30 -0500

    feat(chassis): high-fid sprites with 1000-char chunks (mains)

--- src/assets/chassis/off.js
+++ src/assets/chassis/off.js
@@ -3,4 +3,7 @@ import p1 from "./off_p1.js";
 import p2 from "./off_p2.js";
 import p3 from "./off_p3.js";
 import p4 from "./off_p4.js";
-export default "data:image/png;base64," + p1 + p2 + p3 + p4;
+import p5 from "./off_p5.js";
+import p6 from "./off_p6.js";
+import p7 from "./off_p7.js";
+export default "data:image/png;base64," + p1 + p2 + p3 + p4 + p5 + p6 + p7;
