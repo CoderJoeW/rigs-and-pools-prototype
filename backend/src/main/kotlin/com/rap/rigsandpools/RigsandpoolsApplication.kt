c683f38518b192a4f88dba57a119a0c039c37414
CoderJoe <jwilliamson@jslcodelabs.com>
2026-08-21 18:57:12 -0500

    restructuring with a backend

---
+++ backend/src/main/kotlin/com/rap/rigsandpools/RigsandpoolsApplication.kt
@@ -0,0 +1,11 @@
+package com.rap.rigsandpools
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RigsandpoolsApplication
+
+fun main(args: Array<String>) {
+	runApplication<RigsandpoolsApplication>(*args)
+}
